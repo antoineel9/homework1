9f1d9839ed3bce6111be2414251c188af6f74878
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-08 10:20:35 -0500

    Add files via upload

---
+++ Departments.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD1.Data
+{
+    public class Departments
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DID { get; set; }
+
+        public string DName { get; set; }
+    }
+}
