fd52df399e8e6ee869d106162f0378bd2e46900b
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-08 10:23:23 -0500

    Add files via upload

---
+++ Employees.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD1.Data
+{
+    public class Employees
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int EID { get; set; }
+
+        public string FName { get; set; }
+        public string LName { get; set; }
+
+        [ForeignKey("DID")]
+        public Departments DeptId { get; set; }
+    }
+}
